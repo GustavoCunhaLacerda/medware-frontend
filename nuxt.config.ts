3f150a8e4ea56a8318dc8c8b3734615236a3a54b
Gustavo Cunha Lacerda <gustavocunhalacerda@gmail.com>
2023-10-04 14:58:51 -0300

    feat: composables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  modules: ["@vueuse/nuxt"],
+  css: ["bootstrap/dist/css/bootstrap.css"],
+  plugins: [{ src: "./plugins/bootstrap.ts", mode: "client" }],
+  imports: {
+    dirs: [
+      "composables",
+      "composables/*/index.{ts,js,mjs,mts}",
+      "composables/**",
+    ],
+  },
+});
